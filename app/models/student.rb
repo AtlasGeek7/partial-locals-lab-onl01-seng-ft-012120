ca395dc8ad7fb1baf5e803046ffa7957c27f82c4
Khalid T <khalid.tissire@gmail.com>
2020-04-21 13:43:28 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,6 +9,16 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
+class Student < ActiveRecord::Base
+  has_many :classroom_students
+  has_many :classrooms, through: :classroom_students
 
+  def self.search(query)
+    if query.present?
+      where('NAME like ?', "%#{query}%")
+    else
+      self.all
+    end
+  end
 
 end
